8cbe4b116067df4b9b33b2f9057c01cf5912d84e
mcchow <44908668+mcchow@users.noreply.github.com>
2021-02-03 18:21:05 -0800

    change

--- TriviaGame/Game1.cs
+++ TriviaGame/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using TriviaGameClient.Control;
 
 namespace TriviaGame
 {
@@ -8,6 +9,21 @@ namespace TriviaGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private Texture2D background;
+        private Texture2D button;
+        private Texture2D endbutton;
+
+        public void Startpage()
+        {
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
+            _spriteBatch.Draw(button, new Vector2(300, 240), Color.White);
+
+            _spriteBatch.End();
+        }
 
         public Game1()
         {
@@ -25,9 +41,31 @@ namespace TriviaGame
 
         protected override void LoadContent()
         {
+            // TODO: use this.Content to load your game content here
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            // This is the code we added earlier.
+            background = Content.Load<Texture2D>("stars"); // change these names to the names of your images
+            button = Content.Load<Texture2D>("Button");  // if you are using your own images.
+            var startbotton = new Button(Content.Load<Texture2D>("Button"), Content.Load<SpriteFont>("normal"))
+            {
+                Position = new Vector2(350, 200),
+                Text = "Start"
+            };
+            startbotton.Click += StartButton_Click;
+
+            var quitbotton = new Button(Content.Load<Texture2D>("Button"), Content.Load<SpriteFont>("normal"))
+            {
+                Position = new Vector2(350, 250),
+                Text = "Quit"
+            };
+            quitbotton.Click += StartButton_Click;
+
+        }
+        
+        private void StartButton_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +83,7 @@ namespace TriviaGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            Startpage();
 
             base.Draw(gameTime);
         }
