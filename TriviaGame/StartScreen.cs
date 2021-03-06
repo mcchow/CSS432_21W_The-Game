aa77220416141b89ab4198f4d30bc48e1e66c9fd
mcchow <44908668+mcchow@users.noreply.github.com>
2021-03-05 20:27:38 -0800

    Update waiting text

--- TriviaGame/StartScreen.cs
+++ TriviaGame/StartScreen.cs
@@ -191,7 +191,8 @@ namespace TriviaGameClient
         public void nextPlayerTurn(NextPlayerTurn a, Connection b)
         {
             points = a.curNumCards;
-            waitingtext.Text = "Wait for another player to Answer the question..."
+            
+            waitingtext.Text = "Wait for "+ a.whosTurn + " to Answer the question..."
             stage = "wait";
         }
 
